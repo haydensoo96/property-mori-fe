5cd71cfccb2be33d638891e0eebbd5395077b0f5
Hayden Soo <haydensoo@Haydens-MacBook-Pro.local>
2024-03-01 21:09:26 +0800

    First Commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Contact.css";
+import { BsFillChatDotsFill, BsWhatsapp } from "react-icons/bs";
+const Contact = () => {
+  return (
+    <div id="contact-us" className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className="flexColStart c-left">
+          <span className="orangeText">Our Contact Us</span>
+          <span className="primaryText">Easy to contact us</span>
+          <span className="secondaryText">
+            Contact us and our agent with get in touch with you for more details{" "}
+          </span>
+
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsWhatsapp size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Chat now</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="flexEnd c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
